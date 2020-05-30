a762bf12cd5944231aadda7c6b5ae9fe566c4fc7
Bernara Dzhaparova <57506492+Betty8Bee@users.noreply.github.com>
2020-05-30 19:43:31 -0400

    Updates

---
+++ precache-manifest.9e8ad4a8994c3e4ce421a0fd1b4636d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61436268357d90fd434f99565af4ecb5",
+    "url": "/react-app-lesson5/index.html"
+  },
+  {
+    "revision": "58f43f2dd7ee367af1a5",
+    "url": "/react-app-lesson5/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fd36e7793801b63d0287",
+    "url": "/react-app-lesson5/static/js/2.55cd6844.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-lesson5/static/js/2.55cd6844.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58f43f2dd7ee367af1a5",
+    "url": "/react-app-lesson5/static/js/main.a649d1df.chunk.js"
+  },
+  {
+    "revision": "340fa3ba5f3fbcdccddb",
+    "url": "/react-app-lesson5/static/js/runtime-main.2ba1052b.js"
+  }
+]);
